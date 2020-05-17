def3f47850d1638c519eefc53f32b629eb89a726
Chalanika <chalanikasewwandi@gmail.com>
2020-05-17 11:40:17 +0530

    change text status

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,19 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper =[
+    Icon(
+      Icons.check,
+      color:Colors.green,
+    ),
+  ];
+
+  List<String> questions =[
+    'i want you',
+    ' need you',
+  ];
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -51,6 +64,7 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: FlatButton(
+              textColor: Colors.white,
               color: Colors.green,
               child: Text(
                 'True',
@@ -60,7 +74,15 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ),
+                  );
+                  questionNumber ++;
+                });
               },
             ),
           ),
@@ -83,7 +105,9 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
